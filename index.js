33c7ea170a0084c2be9c3b31cb82e2f4fe7abcb0
php20ai <aliah.iqbal123@hotmail.com>
2024-07-23 09:52:01 +0100

    Addedenvironment variables.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 /// starting Server 
+require('dotenv').config()
 const app = require("./app")
-const port = 3000
-
+const port = process.env.PORT || 5000  // env is a file containing variables (and code)
+// that we do not want it to be visible, i.e. we want it to be kept secret.
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
